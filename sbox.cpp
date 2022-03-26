f58c6c8173669e895f7839ad1a402aadcd2b9fbc
jose-marquez89 <jlmarquez89@gmail.com>
2022-03-25 23:26:22 -0500

    adds char box test

---
+++ sbox.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char string_of_chars[] = "All the chars";
+    string just_a_string = "I'm just a string";
+    cout << string_of_chars << endl;
+    cout << just_a_string << endl;
+
+    return 0;
+}
